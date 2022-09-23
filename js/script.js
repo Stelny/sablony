d8ae89b6799a3434a45f7292e413336055d80bbe
Stelny <jakub.stellner03@gmail.com>
2022-09-23 11:56:16 +0200

    New page -> subpage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    $(".homepage-slider").owlCarousel({
+        loop: true,
+        items: 1,
+        dots: true,
+        nav: false,
+        dotsContainer: "#homepage-slider-dots"
+    })
 
+
+    $("#headerMenuToggle").click(function() {
+        $("#headerMenu").toggleClass("active");
+    })
 })
