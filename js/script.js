862b53fa38089d33659abaaa2cf3a2fc966ec6eb
Stelny <jakub.stellner03@gmail.com>
2022-09-25 12:55:49 +0200

    Section -> Brands

--- js/script.js
+++ js/script.js
@@ -7,6 +7,36 @@ $(document).ready(function(){
         dotsContainer: "#homepage-slider-dots"
     })
 
+    const brands_slider = $('.brands-slider');
+    brands_slider.owlCarousel({
+        loop: true,
+        items: 7,
+        dots: false,
+        nav: false,
+        margin: 20,
+        responsive: {
+            0: {
+                items: 3,
+            },
+            776: {
+                items: 4,
+            },
+            991: {
+                items: 5, 
+            },
+            1200: {
+                items: 7,
+            }
+        }
+    })
+
+    $('.brands-left').click(function() {
+        brands_slider.trigger('prev.owl.carousel');
+    })
+    $('.brands-right').click(function() {
+        brands_slider.trigger('next.owl.carousel')
+    })
+
 
     $("#headerMenuToggle").click(function() {
         $("#headerMenu").toggleClass("active");
