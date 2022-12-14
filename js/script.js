73907b891c149865242c13eb1eba52c43f5a4b67
Stelny <jakub.stellner03@gmail.com>
2022-12-14 19:37:11 +0100

    detail, blog added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+
+    menuToggle.addEventListener("click", function(e) {
+        e.preventDefault();
+        const menu = document.getElementById("menu");
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+            return;
+        }
+        menu.classList.add('active');
+
+    })
 
 })
