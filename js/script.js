6e0c2e3e24888e8eaf7def329af64361e2afefa4
Stelny <jakub.stellner03@gmail.com>
2023-01-04 19:57:40 +0100

    Full page

--- js/script.js
+++ js/script.js
@@ -15,11 +15,58 @@
            faq.classList.add('show');
         })     
     }
+
+    const removeAllClass = (elems, className) => {
+        for (let i = 0; i < elems.length; i++) {
+            elems[i].parentElement.classList.remove(className);
+        }
+    }
+    
+    const changeClassById = (className, className2, elem) => {
+        if (elem.classList.contains(className)) {
+            elem.classList.add(className2);
+            elem.classList.remove(className);
+            return;
+        }
+        elem.classList.add(className);
+        elem.classList.remove(className2);
+    }
+    
+    const changeHeaderColor = () => {
+        setInterval(() => {
+            const logo = document.getElementById("changeLogo");
+            const logoGold = document.getElementById("changeLogoGold");
+            const logoMobile = document.getElementById("changeLogoMobile");
+            const logoGoldMobile = document.getElementById("changeLogoMobileGold");
+            const btn = document.getElementById("changeBtn");
+            changeClassById("btn-pink", "btn-gold", btn);
+            idChange(logo, logoGold);
+            idChange(logoMobile, logoGoldMobile);
+        }, [10000])        
+    }
+    
+
+    changeHeaderColor();
+
+    const openHeader = document.getElementById("openHeader");
+    const headerMenu = document.getElementById("headerMenu");
+
+    openHeader.addEventListener("click", function() {
+        if (headerMenu.classList.contains("active")) {
+            headerMenu.classList.remove('active');
+            return;
+        }
+        headerMenu.classList.add('active');
+    });
+
 })();
 
-
-const removeAllClass = (elems, className) => {
-    for (let i = 0; i < elems.length; i++) {
-        elems[i].parentElement.classList.remove(className);
+const idChange = (logo, logoGold) => {
+    if (logo.classList.contains("active")) {
+        logo.classList.remove('active');
+        logoGold.classList.add('active');
+    } else {
+        logoGold.classList.remove('active');
+        logo.classList.add('active');
     }
 }
