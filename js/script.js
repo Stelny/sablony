a3dcbd43646b87c7844d0e68b634aec53e5018de
Stelny <jakub.stellner03@gmail.com>
2023-01-04 13:48:42 +0100

    Homepage added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
 
-})
+
+(function() {
+    const elems = document.getElementsByClassName("faq-item-question");
+    for (let i = 0; i < elems.length; i++) {
+        const element = elems[i];
+        element.addEventListener("click", function(e) {
+           const faq = e.target.parentElement;
+           let exists = false;
+           if (faq.classList.contains("show")) {
+                exists = true;
+            }
+           removeAllClass(elems, "show")
+           if (exists) return;
+           faq.classList.add('show');
+        })     
+    }
+})();
+
+
+const removeAllClass = (elems, className) => {
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].parentElement.classList.remove(className);
+    }
+}
