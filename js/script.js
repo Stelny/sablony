b4bdbcf25ad47a41e3e98b86e31f11efe3d6f6ff
Jakub Štellner <stelljak@fit.cvut.cz>
2023-01-20 23:37:35 +0100

    Homepage: Header, About, Services, Banner

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
 
+    const headerMenuToggle = document.getElementById("headerMenuToggle");
+    const headerMenu  = document.getElementById("headerMenu");
+
+    headerMenuToggle.addEventListener("click", function() {
+        if (headerMenu.classList.contains("active")) {
+            headerMenu.classList.remove("active");
+            return;
+        }
+        headerMenu.classList.add("active");
+    });
+
+    $('.homepage-slider').owlCarousel({
+        loop:false,
+        nav:false,
+        items: 1
+    })
+
+    removeEventListener("click", headerMenuToggle);
 })
+
