86dd81c435cbf542eb6c1594d18068a7907ca187
Stelny <jakub.stellner03@gmail.com>
2023-01-21 14:09:14 +0100

    Subpage: Complete

--- js/script.js
+++ js/script.js
@@ -17,6 +17,29 @@ $(document).ready(function(){
         items: 1
     })
 
+    $('.reference-slider').owlCarousel({
+        loop:true,
+        nav: true,
+        items: 1,
+        margin: 20,
+        dots: false,
+        responsive: {
+            1200: {
+                items: 5
+            },
+            991: {
+                items: 4
+            },
+            776: {
+                items: 3
+            },
+            600: {
+                items: 2
+            }
+
+        }
+    })
+
     removeEventListener("click", headerMenuToggle);
 })
 
