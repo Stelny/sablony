8615798322b9286567f36bc4fd0611b9f794b788
Stelny <jakub.stellner03@gmail.com>
2023-03-08 15:42:28 +0100

    Newsletter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+const headerMenu = document.getElementById("header-menu");
+const headerToggle = document.getElementById("header-toggle");  
+
+headerToggle.addEventListener("click", function(){
+    const is_active = headerMenu.classList.contains('active');
+    if (is_active) {
+        headerMenu.classList.remove('active');
+        return;
+    }
+    
+    headerMenu.classList.add('active');
 
 })
+
+
+headerToggle.removeEventListener("click");
