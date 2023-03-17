85716f469924709467d905728b8eb54a64f593fc
Jakub Štellner <stelljak@fit.cvut.cz>
2023-03-17 13:27:32 +0100

    Hero && Header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
 $(document).ready(function(){
-    $('.hero-slider').owlCarousel({
+
+
+    const heroSlider = $('.hero-slider')
+    heroSlider.owlCarousel({
         loop:true,
         margin: 0,
         nav: false,
-        items: 1
+        dots: true,
+        items: 1,
+        singleItem: true,
+        stagePadding: 0,
+        dotsContainer: "#banner-dots",
+        animateOut: 'fadeOut'
+        
     })
+
+
+    const headerMenu = document.getElementById("headerMenu");
+    const headerMenuOpen = document.getElementById("headerMenuOpen"); 
+    const headerMenuClose = document.getElementById("headerMenuClose");
+
+    headerMenuOpen.addEventListener("click", function() {
+        headerMenu.classList.add('active');
+    })
+
+    headerMenuClose.addEventListener("click", function() { 
+        headerMenu.classList.remove('active');
+    });
+
+    $(".prevHero").click(function(){
+        heroSlider.trigger("prev.owl.carousel");
+    })
+    $(".nextHero").click(function(){
+        heroSlider.trigger("next.owl.carousel");
+    })
+
+
+    removeEventListener("click", headerMenuClose);
+    removeEventListener("click", headerMenuOpen)
 })
