b263e82a4e32d2b143234ca6aba7171939ae518a
Jakub Štellner <jakub.stellner03@gmail.com>
2024-10-29 13:17:44 +0100

    example header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function () {
+    const headerMenu = document.getElementById("headerMenu");
+    const headerMenuToggle = document.getElementById("headerMenuToggle");
 
-})
+    headerMenuToggle.addEventListener("click", function () {
+        headerMenu.classList.toggle("active");
+    });
+});
