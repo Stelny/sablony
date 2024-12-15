97e3c3051d49f7a5989f5ff07e4d7ac3a6904196
Jakub Štellner <jakub.stellner03@gmail.com>
2024-12-15 22:28:51 +0100

    homepage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
 function handleHeader() {
     const header = document.querySelector(".header");
+    const toggleHeader = document.getElementById("toggle-header");
+    const menuHeader = document.getElementById("menu-header");
 
+    // Handle scroll behavior for the header
     window.addEventListener("scroll", () => {
         if (window.scrollY > 0) {
             header.classList.add("scrolling");
@@ -10,15 +12,22 @@ function handleHeader() {
         }
     });
 
-
-    const toggleHeader = document.getElementById("toggle-header");
-    const menuHeader = document.getElementById("menu-header");
+    // Handle toggle header menu
     toggleHeader.addEventListener("click", () => {
-        menuHeader.classList.toggle('active');
+        const isActive = menuHeader.classList.toggle("active");
         toggleHeader.classList.toggle("active");
-    })
-}
 
+        if (isActive) {
+            // Disable body scroll and enable scrolling in the header menu
+            document.body.style.overflow = "hidden";
+            menuHeader.style.overflowY = "auto";
+        } else {
+            // Re-enable body scroll
+            document.body.style.overflow = "";
+            menuHeader.style.overflowY = "";
+        }
+    });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     handleHeader();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hlavní strana
+
+Obrázky v pozadích jednotlivých sekcí mají různé opacity, to asi nebude editovatelné, tak doporučuji nahrávat obrázky už upravené. 
