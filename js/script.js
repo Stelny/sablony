b31f945f7bdc44ef7630ab17e7d7c6016ce5e6b0
Jakub Štellner <jakub.stellner03@gmail.com>
2025-02-20 22:46:11 +0100

    prepare HP && complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function handleHeader() {
     const header = document.querySelector(".header");
+    const headerToggle = document.getElementById("header-toggle");
+
+    headerToggle.addEventListener("click", () => {
+        header.classList.toggle("active");
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -2,28 +2,6 @@
 tailwind.config = {
     theme: {
         extend: {
-            screens: {
-                xs: "350px",
-                sm: "640px",
-                md: "768px",
-                lg: "1024px",
-                xl: "1280px",
-                xxl: "1400px",
-                "2xl": "1536px",
-            },
-            container: {
-                center: true,
-                padding: "1.25rem",
-                screens: {
-                    xs: "350px",
-                    sm: "640px",
-                    md: "768px",
-                    lg: "1024px",
-                    xl: "1280px",
-                    xxl: "1400px",
-                    "2xl": "1536px",
-                },
-            },
             colors: {
                 customBlack: "#393939",
                 purple: "#44008B",
@@ -31,7 +9,7 @@ tailwind.config = {
                 tercia: "#17E0FF",
             },
             backgroundImage: {
-
+                gradientHero: "url(/images/hero/gradient.svg)"
             },
             boxShadow: {
                 card: "0px 0px 35px #0000000D"
